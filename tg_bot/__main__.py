bdb87673ec8940c2ee2ba2328169c7ed92591822
cinemaFactori <64655954+cinemaFactori@users.noreply.github.com>
2020-05-05 17:28:20 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,9 +18,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-
-
 https://t.me/Cf_chat
+
+
 
 
 
