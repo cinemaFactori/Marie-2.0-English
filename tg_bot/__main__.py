d2420d250a0139e3cc558669a7de9d9716ad9db9
cinemaFactori <64655954+cinemaFactori@users.noreply.github.com>
2020-05-05 17:26:34 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,9 +18,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-no one gonna help you 🤣🤣🤣🤣
 
 
+https://t.me/Cf_chat
 
 
 
