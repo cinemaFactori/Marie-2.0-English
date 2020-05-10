e130a92b3e515ee242d4d14ef22416a67e3bbe51
cinemaFactori <64655954+cinemaFactori@users.noreply.github.com>
2020-05-10 23:13:54 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi Iam Alia Bot
+no one gonna help you 🤣🤣🤣🤣
 
 
 
