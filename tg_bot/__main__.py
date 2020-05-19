cd0078729421d021967edcfeb0ae7b9e26b5e281
cinemaFactori <64655954+cinemaFactori@users.noreply.github.com>
2020-05-19 13:40:27 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-[hi iam alia group management robot 😎](https://t.me/Berlinhere)
+[no one gonna help you 🤣🤣🤣🤣](https://t.me/Berlinhere)
 
 
 
